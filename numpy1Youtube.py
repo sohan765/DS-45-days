ecb5eef50ccd7951546442ddab72923d73f2343a
sohan765 <patelsohanlal416@gmail.com>
2026-07-20 23:54:03 +0530

    Update numpy1Youtube.py

--- numpy1Youtube.py
+++ numpy1Youtube.py
@@ -7,18 +7,18 @@ data = {
     "Salary" : [200000,30000,15000,102233]
 }
 df = pd.DataFrame(data)
+print(df)
+print(df["name"])
+
+df["destination"] = ["paris","mumbai","kolkata","USA"]
 # print(df)
-# print(df["name"])
+print(df.drop("destination",axis= 1))
+print(df.drop(2,axis=0))
 
-# df["destination"] = ["paris","mumbai","kolkata","USA"]
-# # print(df)
-# print(df.drop("destination",axis= 1))
-# print(df.drop(2,axis=0))
+print(df.loc[[0,1]])
+print(df.iloc[3])
 
-# print(df.loc[[0,1]])
-# print(df.iloc[3])
-
-# print(df.loc[[0,1]][["Age","Salary"]])
+print(df.loc[[0,1]][["Age","Salary"]])
 
 data1 = {
     "A" :[1,2,np.nan,4,5],
@@ -27,19 +27,19 @@ data1 = {
     "D" : [np.nan,2,np.nan,np.nan,5]
 }
 df1 = pd.DataFrame(data1)
-# print(df1)
-# print(df1.isna())
-# print(df1.isna().sum())
-# print(df1.isna().any())
-# it is work on the rows
-# print(df1.dropna())
-# print(df1.dropna(thresh=3))
+print(df1)
+print(df1.isna())
+print(df1.isna().sum())
+print(df1.isna().any())
+it is work on the rows
+print(df1.dropna())
+print(df1.dropna(thresh=3))
 
 # fillna is work on the bases of column
-# print(df1.fillna(0))
+print(df1.fillna(0))
 values = {"A":10,"B":20,"C":30,"D":40}
 print(df1.fillna(value=values,inplace = True))
-# print(df1)
-# print(df1["A"].mean())
+print(df1)
+print(df1["A"].mean())
 
 print(df1['A'].agg(['mean','max','min','sum']))
